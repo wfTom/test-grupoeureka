00833753bb155d70fabecbd267a8be60e0108ae0
wfTom <wf_tom@hotmail.com.br>
2021-05-17 13:44:14 -0300

    add environment variable to requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,9 @@
 import axios, { AxiosResponse } from 'axios'
 
-// const api = axios.create({
-//   baseURL: 'http://localhost:3003'
-// })
-
 export default async function api<T = any>(
   path: string
 ): Promise<AxiosResponse<T>> {
-  return axios.get<T>('http://localhost:3000/api/' + path)
+  // return axios.get<T>('http://localhost:3000/api/' + path)
 
-  // return axios.get<T>(process.env.NEXT_PUBLIC_URL + 'api/' + path)
+  return axios.get<T>(process.env.NEXT_PUBLIC_URL + 'api/' + path)
 }
